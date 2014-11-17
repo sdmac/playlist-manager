97d8b5f565ffd27bf9ad662d2f767eba29fca163
sdmac <seanmac@ucla.edu>
2014-11-17 11:25:33 -0800

    Use source-specific playlist parser/fetcher and add impl for KCRW

---
+++ kcrw.py
@@ -0,0 +1,137 @@
+import os
+import time
+import requests
+import htmlentitydefs
+
+from playlist import PlaylistEntry, Playlist, merge_playlists
+
+from HTMLParser import HTMLParser
+
+
+class PlaylistParser(HTMLParser):
+
+    def __init__(self, playlist):
+        HTMLParser.__init__(self)
+        self.last_tag = None
+        self.last_attr = None
+        self.entry = PlaylistEntry()
+        self.playlist = playlist
+        self.search_date = ''
+        self.data = ''
+
+    def handle_starttag(self, tag, attrs):
+        self.last_tag = tag
+        if tag == 'td':
+            for name, value in attrs:
+                if name == 'class' and value in self.entry.fields:
+                    self.last_attr = value
+                    setattr(self.entry, value, None)
+                if (name == 'class' and value == 'search_date'
+                        and not self.search_date):
+                    self.last_attr = value
+
+    def handle_endtag(self, tag):
+        if tag == 'td' and self.last_attr in self.entry.fields:
+            setattr(self.entry, self.last_attr, self.data.strip())
+            self.data = ''
+            if self.entry.ready():
+                self.entry.play_count = 1
+                self.playlist.append(self.entry)
+                self.entry = PlaylistEntry()
+                self.last_tag = None
+                self.last_attr = None
+        if tag == 'td' and self.last_attr == 'search_date':
+            self.data = ''
+
+    def handle_data(self, data):
+        if self.last_tag == 'td' and (self.last_attr in self.entry.fields 
+                                      or self.last_attr == 'search_date'):
+            if data:
+                if self.last_attr == 'search_date' and not self.search_date:
+                    self.search_date = data.strip()
+                self.data = self.data + data
+
+    def handle_entityref(self, name):
+        if self.last_tag == 'td' and (self.last_attr in self.entry.fields
+                                      or self.last_attr == 'search_date'):
+            codepoint = htmlentitydefs.name2codepoint[name]
+            self.data = self.data + unichr(codepoint)
+
+
+class PlaylistFetcher(object):
+    url = 'http://newmedia.kcrw.com/tracklists/search.php'
+
+    def __init__(self):
+        self.max_start = 300
+
+    def fetch(self, date):
+        start = 0
+        playlist = []
+        parser = PlaylistParser(playlist)
+        while start < self.max_start:
+            playlist_size = len(playlist)
+            self._fetch(parser, date, start)
+            if len(playlist) == playlist_size:
+                break
+            start = start + 50
+        try:
+            sd = time.strptime(parser.search_date, "%A, %B %d, %Y")
+            actual_date = "{0}/{1}/{2}".format(sd.tm_mon,
+                                               sd.tm_mday,
+                                               sd.tm_year)
+            return (playlist, actual_date)
+        except ValueError:
+            return (None, None)
+
+    def _fetch(self, parser, date, start):
+        params = {'search_type': '0',
+                  'date_from': date,
+                  'start': start}
+        this_url = ''
+        def grab_url(r, *args, **kwargs):
+            this_url = r.url
+        r = requests.get(self.url, params=params,
+                         timeout=2, stream=True,
+                         hooks=dict(response=grab_url))
+        if r.status_code != 200:
+            raise RuntimeError('GET failed with params=%s' % params)
+        r.encoding = 'utf-8'
+        parser.feed(r.text)
+
+
+class PlaylistUpdater(object):
+    file_prefix = 'kcrw_playlist_'
+    file_suffix = '.json'
+
+    def __init__(self):
+        self.fetcher = PlaylistFetcher()
+
+    def update(self, year, month, day):
+        play_list = None
+        file_name = "{0}{1}{2}{3}".format(self.file_prefix,
+                                          year, month,
+                                          self.file_suffix)
+        if os.path.exists(file_name):
+            play_list = Playlist()
+            play_list.from_json_file(file_name)
+        else:
+            play_list = Playlist(source='kcrw',
+                                 name='%s%s' % (year, month),
+                                 frequency='daily')
+        the_date = "{0}/{1}/{2}".format(month, day, year)
+        if the_date in play_list.records:
+            print "Playlist for {0} already exists".format(the_date)
+            return -1
+        else:
+            print "Fetching playlist for new date {0}".format(the_date)
+            (p_list, search_date) = self.fetcher.fetch(the_date)
+            if search_date == the_date:
+                print ("New date is valid, merging into {0} playlist"
+                        .format(file_name))
+                play_list.playlist = merge_playlists(play_list.playlist, p_list)
+                play_list.records.append(the_date)
+                play_list.to_json_file(file_name)
+                return 0
+            else:
+                print ("Latest date available is {0}".format(search_date))
+                return 1
--- playlist.py
+++ playlist.py
@@ -1,12 +1,6 @@
-#!/usr/bin/python
-
 import json
-import requests
-import htmlentitydefs
 
 from difflib import SequenceMatcher
-
-from HTMLParser import HTMLParser
 
 
 class PlaylistEntry(object):
@@ -31,84 +25,36 @@ class PlaylistEntry(object):
         return str(self)
 
 
-class PlaylistParser(HTMLParser):
-
-    def __init__(self, playlist):
-        HTMLParser.__init__(self)
-        self.last_tag = None
-        self.last_attr = None
-        self.entry = PlaylistEntry()
-        self.playlist = playlist
-        self.data = ''
-
-    def handle_starttag(self, tag, attrs):
-        self.last_tag = tag
-        if tag == 'td':
-            for name, value in attrs:
-                if name == 'class' and value in self.entry.fields:
-                    self.last_attr = value
-                    setattr(self.entry, value, None)
-
-    def handle_endtag(self, tag):
-        if tag == 'td' and self.last_attr in self.entry.fields:
-            setattr(self.entry, self.last_attr, self.data.strip())
-            self.data = ''
-            if self.entry.ready():
-                self.playlist.append(self.entry)
-                self.entry = PlaylistEntry()
-                self.last_tag = None
-                self.last_attr = None
-
-    def handle_data(self, data):
-        if self.last_tag == 'td' and self.last_attr in self.entry.fields:
-            if data:
-                self.data = self.data + data
-
-    def handle_entityref(self, name):
-        if self.last_tag == 'td' and self.last_attr in self.entry.fields:
-            codepoint = htmlentitydefs.name2codepoint[name]
-            self.data = self.data + unichr(codepoint)
-
-
 class PlaylistEntryEncoder(json.JSONEncoder):
 
     def default(self, playlist):
         return playlist.__dict__
 
 
-class PlaylistFetcher(object):
+class Playlist(object):
 
-    def __init__(self, url):
-        self.url = url
-        self.max_start = 200
+    def __init__(self, playlist=[], source=None,
+                 name=None, frequency=None, records=[]):
+        self.playlist = playlist
+        self.source = source
+        self.name = name
+        self.frequency = frequency
+        self.records = records
+
+    def to_json_file(self, file_name):
+        with open(file_name, 'w') as o:
+            json.dump(self, o, cls=PlaylistEntryEncoder)
+
+    def from_json_file(self, file_name):
         self.playlist = []
-        self.parser = PlaylistParser(self.playlist)
-
-    def fetch(self, date):
-        start = 0
-        while start < self.max_start:
-            playlist_size = len(self.playlist)
-            self._fetch(date, start)
-            if len(self.playlist) == playlist_size:
-                break
-            start = start + 50
-        return self.playlist
-
-    def _fetch(self, date, start):
-        params = {'search_type': '0',
-                  'date_from': date,
-                  'start': start}
-        this_url = ''
-        def grab_url(r, *args, **kwargs):
-            this_url = r.url
-        try:
-            r = requests.get(self.url, params=params,
-                             timeout=2, stream=True,
-                             hooks=dict(response=grab_url))
-            r.encoding = 'utf-8'
-            self.parser.feed(r.text)
-        except requests.exceptions.Timeout:
-            print 'Request timeout for: {0}'.format(this_url)
+        with open(file_name, 'r') as i:
+            j = json.load(i)
+            for attr_name in self.__dict__:
+                if attr_name == 'playlist':
+                    for e in j[attr_name]:
+                        self.playlist.append(PlaylistEntry(e))
+                else:
+                    setattr(self, attr_name, j[attr_name])
 
 
 def merge_playlists(old, new):
